42e76dca4e84d657ec3ed7c89233195ab5699f42
Abhi22042004 <123793235+Abhi22042004@users.noreply.github.com>
2024-01-05 23:29:49 +0530

    Update untoldcoding.js

--- untoldcoding.js
+++ untoldcoding.js
@@ -5,17 +5,24 @@ const card = document.querySelector(".card");
 const handleButtonClick = (e) => {
   const targetSection = e.target.getAttribute("data-section");
   const section = document.querySelector(targetSection);
+  
+  // Check if the target section is "#about" and remove "is-active" class accordingly
   targetSection !== "#about"
     ? card.classList.add("is-active")
     : card.classList.remove("is-active");
+  
   card.setAttribute("data-state", targetSection);
   sections.forEach((s) => s.classList.remove("is-active"));
   buttons.forEach((b) => b.classList.remove("is-active"));
   e.target.classList.add("is-active");
   section.classList.add("is-active");
+
+  // Open the link when the "Submit" button is clicked
+  if (targetSection === "#submit") {
+    window.location.href = "https://forms.gle/515Bb7owo6hmrPKt5";
+  }
 };
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", handleButtonClick);
 });
-
